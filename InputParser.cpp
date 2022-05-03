0343bee0a52cbcf57a822fcce3b075920ae71424
Charles Kelley <chas.kelley21@gmail.com>
2022-05-02 20:45:52 -0500

    inputParser starter

---
+++ InputParser.cpp
@@ -0,0 +1,16 @@
+#include "InputParser.h"
+
+// constructor to set filename of the class
+InputParser::InputParser(std::string fileName) {
+    // todo: implement constructor
+}
+
+// setter to change the file name if need be. modifies filename class member
+void InputParser::setFilename(std::string newFile) {
+    // todo: implement setter for member [filename]
+}
+
+// for now, read in every line from [filename] class member
+void InputParser::readFile() {
+    // todo: implement method to read in each line of [filename] and print to cout
+}
