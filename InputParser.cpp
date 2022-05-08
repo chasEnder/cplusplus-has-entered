6d8f522ac69f94853a391f084a94fc6d2bda7ee9
Marat Rosencrants <rosencrantsmc@gmail.com>
2022-05-07 22:32:48 -0500

    constructor and read file code

--- InputParser.cpp
+++ InputParser.cpp
@@ -1,22 +1,25 @@
 #include "InputParser.h"
 
 // constructor to set filename of the class
-InputParser::InputParser(std::string fileName) {
+// have unique syntax
+InputParser::InputParser(std::string newFile) {
     // todo: implement constructor
-    //std::cout << "Please input name of file to be read: " << end1;
-    //std::cin >> fileName;
+    fileName = newFile;
+    
 }
+
+// difference between these two?
 
 // setter to change the file name if need be. modifies filename class member
 void InputParser::setFilename(std::string newFile) {
     // todo: implement setter for member [filename]
-    
+    fileName = newFile;
 }
 
 // getter for the file name if need be. does not modify filename class member
-std::string getFilename(std::string newFile) {
+std::string InputParser::getFilename() {
     // todo: implement getter for member [filename]
-    return "";
+    return fileName;
 }
 
 // for now, read in every line from [filename] class member
@@ -24,5 +27,21 @@ std::string getFilename(std::string newFile) {
 //          false if failed or no [filename]
 bool InputParser::readFile() {
     // todo: implement method to read in each line of [filename] and print to cout
+    std::ifstream myfile(fileName); // might be wrong
+    
+    std::string line;
+
+    if (myfile.is_open()) {
+        while (std::getline(myfile,line)) {
+            std::cout << line << std::endl;
+        }
+        myfile.close();
+    }
+
+    else {
+        std::cout << "Unable to open file" << std::endl;
+        return false;
+    }
+
     return false;
 }
